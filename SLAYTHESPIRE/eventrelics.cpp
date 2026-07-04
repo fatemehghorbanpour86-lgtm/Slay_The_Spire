bfdd5c190916ee4e1ca07dc5c4dfdf82011465ae
Anaconda1385 <anahitaamerifar85@gmail.com>
2026-07-04 17:45:03 +0330

    added MutagenicStrength Relic

--- SLAYTHESPIRE/eventrelics.cpp
+++ SLAYTHESPIRE/eventrelics.cpp
@@ -1,5 +1,12 @@
 #include "eventrelics.h"
 
+#include "player.h"
+#include "effect.h"
+
+//======================================================
+//  WarpedTongs
+//======================================================
+
 WarpedTongs::WarpedTongs()
     : Relic("Warped Tongs",
    "At the start of combat, upgrade a random card for the rest of this combat.",
@@ -22,4 +29,42 @@ void WarpedTongs::onCombatStart(Player* player)
     //  - restoring the original card after combat.
 
     // Example: player->getCombatDeck()->upgradeRandomCardForCombat();
+}
+
+
+//======================================================
+//  MutagenicStrength
+//======================================================
+
+
+MutagenicStrength::MutagenicStrength()
+    : Relic("Mutagenic Strength",
+            "At the start of combat gain 3 Strength and lose it after that turn. ",
+            Relic::Tier::Event),
+
+    strengthActive(false)
+{
+}
+
+void MutagenicStrength::onCombatStart(Player* player)
+{
+    if (!player)
+        return;
+
+    player->addEffect(Effect::Type::Strength,Effect::Category::Buff,3);
+
+    strengthActive = true;
+}
+
+void MutagenicStrength::onTurnEnd(Player* player)
+{
+    if (!player)
+        return;
+
+    if (!strengthActive)
+        return;
+
+    player->addEffect(Effect::Type::Strength,Effect::Category::Buff,-3);
+
+    strengthActive = false;
 }
--- SLAYTHESPIRE/eventrelics.h
+++ SLAYTHESPIRE/eventrelics.h
@@ -3,12 +3,26 @@
 
 #include "relic.h"
 
+class Player;
+
 class WarpedTongs : public Relic
 {
 public:
     WarpedTongs();
 
     void onCombatStart(Player* player) override;
+};
+
+class MutagenicStrength : public Relic
+{
+public:
+    MutagenicStrength();
+
+    void onCombatStart(Player* player) override;
+    void onTurnEnd(Player* player) override;
+
+private:
+    bool strengthActive;
 };
 
 #endif // EVENTRELICS_H
