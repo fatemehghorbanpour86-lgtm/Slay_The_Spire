3f84d34ba04e3134108963c162b02118317df27a
fatemehghorbanpour86-lgtm <fatemehghorbanpour86@gmail.com>
2026-07-06 01:09:00 +0330

    Update backGround and Buttons of loginpage

--- SLAYTHESPIRE/loginpage.cpp
+++ SLAYTHESPIRE/loginpage.cpp
@@ -1,12 +1,23 @@
 #include "loginpage.h"
 #include <QVBoxLayout>
 #include <QBoxLayout>
+#include <QLabel>
+#include <QMovie>
 
 loginpage::loginpage(QWidget *parent)
     : QWidget{parent}
 {
     setObjectName("loginPage");
-    setStyleSheet("QWidget#loginPage {border-image: url(:/loginpage.png); }"
+
+    QLabel *bg = new QLabel(this);
+    bg->setGeometry(0, 0, 1280, 720);
+    bg->setScaledContents(true);
+    bg->lower();
+    QMovie *movie = new QMovie(":/LoginPage.gif");
+    bg->setMovie(movie);
+    movie->start();
+
+    setStyleSheet(
                   "QToolTip { color: #facc15; background-color: #1f2937; border: 1px solid #b91c1c;"
                   "border-radius: 4px; padding: 6px; font-weight: bold; font-family: Tahoma;"
                   "}"
@@ -48,14 +59,24 @@ loginpage::loginpage(QWidget *parent)
     buttonsLayout->setAlignment(Qt::AlignCenter);
 
     loginBtn = new QPushButton(this);
-    loginBtn->setFixedSize(120, 120);
+    loginBtn->setCursor(Qt::PointingHandCursor);
+    loginBtn->setFixedSize(100, 100);
     loginBtn->setToolTip("If you have an account, Press this.");
-    loginBtn->setStyleSheet("QWidget {border-image: url(:/LoginBtn.png); }");
+    loginBtn->setStyleSheet(
+        "QWidget {border-image: url(:/LoginBtn.png); }"
+        "QPushButton:pressed { "
+        "   margin: 5px 8px 5px 8px; "
+        "}");
 
     registerBtn = new QPushButton(this);
-    registerBtn->setFixedSize(120, 120);
-    registerBtn->setToolTip("IF you dont have account, Press this.");
-    registerBtn->setStyleSheet("QWidget {border-image: url(:/RegisterBtn.png); }");
+    registerBtn->setCursor(Qt::PointingHandCursor);
+    registerBtn->setFixedSize(100, 100);
+    registerBtn->setToolTip("If you dont have account, Press this.");
+    registerBtn->setStyleSheet(
+        "QWidget {border-image: url(:/RegisterBtn.png); }"
+        "QPushButton:pressed { "
+        "   margin: 5px 8px 5px 8px; "
+        "}");
 
     buttonsLayout->addWidget(loginBtn);
     buttonsLayout->addWidget(registerBtn);
