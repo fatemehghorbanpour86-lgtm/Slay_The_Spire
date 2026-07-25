6eb0ca8cd67100126fd8fe82d47b7658327cdf67
fatemehghorbanpour86-lgtm <fatemehghorbanpour86@gmail.com>
2026-07-25 23:14:01 +0330

    add custom cursor

--- SLAYTHESPIRE/main.cpp
+++ SLAYTHESPIRE/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "audiomanager.h"
 #include <QApplication>
+#include <QPixmap>
+#include <QCursor>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     AudioManager::instance().initialize();
+
+    QPixmap pixmap(":/cursor.png");
+    QPixmap scaledPixmap = pixmap.scaled(30, 40, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QCursor customCursor(scaledPixmap, 0, 0);
+
+    QApplication::setOverrideCursor(customCursor);
+
     MainWindow w;
     w.show();
     return QApplication::exec();
